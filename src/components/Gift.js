fa760db6a2d1963eab26b029bc6debc1c665e0f7
Achmad Miftahudin <93859870+myth2310@users.noreply.github.com>
2022-04-20 09:13:58 +0700

    Update Gift.js
    Update Gift.js

--- src/components/Gift.js
+++ src/components/Gift.js
@@ -1,39 +1,39 @@
 import './Gift.css';
-import React from 'react';
-import Card from '@mui/material/Card';
-import Typography from '@mui/material/Typography';
-import CardMedia from '@mui/material/CardMedia';
-import {CardActionArea } from '@mui/material';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
+// import React from 'react';
+// import Card from '@mui/material/Card';
+// import Typography from '@mui/material/Typography';
+// import CardMedia from '@mui/material/CardMedia';
+// import {CardActionArea } from '@mui/material';
+// import Grid from '@mui/material/Grid';
+// import Container from '@mui/material/Container';
 
 const Gift = (props) => {
   // const [count, setCount] = useState(0);
     return (
-      <Container>
-       <Grid >
-        <Card sx={{ maxWidth: 345, mb: "30px", mt:"30px"}}>
-          <CardActionArea>
-          <CardMedia
-              component="img"
-              height="194"
-              image={props.url}
-            />
-            <Typography sx={{ fontSize: 15 }} color="text.success" gutterBottom>
-            {props.title}
-            </Typography>
-          </CardActionArea> 
-        </Card>
-        </Grid>
-      </Container>
+      // <Container>
+      //  <Grid >
+      //   <Card sx={{ maxWidth: 345, mb: "30px", mt:"30px"}}>
+      //     <CardActionArea>
+      //     <CardMedia
+      //         component="img"
+      //         height="194"
+      //         image={props.url}
+      //       />
+      //       <Typography sx={{ fontSize: 15 }} color="text.success" gutterBottom>
+      //       {props.title}
+      //       </Typography>
+      //     </CardActionArea> 
+      //   </Card>
+      //   </Grid>
+      // </Container>
       
 
-          // <div className='Card'>
-          //   <img src={props.url} alt="gif"></img>
-          //   <div className='container'>
-          //     <p>{props.title}</p>
-          //   </div>  
-          // </div>  
+          <div className='Card'>
+            <img src={props.url} alt="gif"></img>
+            <div className='container'>
+              <p>{props.title}</p>
+            </div>  
+          </div>  
     );
   };
   
