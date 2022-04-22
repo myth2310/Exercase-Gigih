c4f51d99aea7b0021a7ba3c4130e202153bd1b87
Achmad Miftahudin <93859870+myth2310@users.noreply.github.com>
2022-04-22 15:30:35 +0700

    Update index.js
    Update index.js

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -18,7 +18,7 @@ class Sample extends Component {
   
   handleSubmit = (e) => {
     e.preventDefault();
-    let apiKey = process.env.REACT_APP_GIPHY;
+    let apiKey = process.env;
     let query = this.state.searchQuerry ;
     let limit = 12;
     let baseUrl = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=${limit}&q=${query}`;
